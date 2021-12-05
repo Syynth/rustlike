f5497f7783791e1c690fc3f3d21d72cc7769e21d
syynth <ben.cochrane2112@gmail.com>
2021-12-05 15:21:59 -0500

    Refactors movement to use message system

--- src/components.rs
+++ src/components.rs
@@ -20,3 +20,9 @@ pub struct Health {
     pub max: i32,
     pub current: i32,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct WantsToMove {
+    pub entity: Entity,
+    pub destination: Point,
+}
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -5,12 +5,12 @@ use crate::prelude::*;
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState,
 ) {
-    if let Some(key) = key {
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+    if let Some(key) = *key {
         let delta = match key {
             VirtualKeyCode::W | VirtualKeyCode::Up => Point::new(0, -1),
             VirtualKeyCode::S | VirtualKeyCode::Down => Point::new(0, 1),
@@ -18,16 +18,16 @@ pub fn player_input(
             VirtualKeyCode::D | VirtualKeyCode::Right => Point::new(1, 0),
             _ => Point::new(0, 0),
         };
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query().filter(component::<Player>());
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            });
-        }
+        players.iter(ecs).for_each(|(entity, pos)| {
+            let destination = *pos + delta;
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+        });
+        *turn_state = TurnState::PlayerTurn;
     }
 }
